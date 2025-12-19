8aa851bcc01a3da42ac00f2338fc900346801a8f
Luismigonza <luismigonzalez38@gmail.com>
2025-12-18 21:28:30 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// MENU RESPONSIVE
+var btnMenuOpen = document.getElementById("btnMenuOpen"),
+    btnMenuClose = document.getElementById("btnMenuClose"),
+    menuResponsive = document.getElementById("menuBar"),
+    enlaces = document.getElementById("enlaces");
+
+    // Click abrir
+    btnMenuOpen.addEventListener("click", function () {
+        menuResponsive.classList.add("active");
+    });
+
+    // Click cerrar
+    btnMenuClose.addEventListener("click", function () {
+        menuResponsive.classList.remove("active");
+    });
+
+    // Cerrar menu con elementos de enlace
+    enlaces.addEventListener("click", function () {
+        menuResponsive.style.transitionDelay = "0.5s";
+        menuResponsive.classList.remove("active");
+    });
+
+
